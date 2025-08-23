2892876996e4c21ebf4ae85786d7c656222f7b88
bamboechop <info@bamboechop.at>
2025-08-23 13:21:17 +0200

    feat: add VideoComponent

---
+++ src/VideoComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="h-screen max-w-twitch-iframe mx-auto">
+    <h1>Video Component</h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/video_component.ts
@@ -0,0 +1,6 @@
+import './assets/base.css'
+
+import { createApp } from 'vue'
+import VideoComponent from './VideoComponent.vue'
+
+createApp(VideoComponent).mount('#video_component')
